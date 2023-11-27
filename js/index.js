de0bf621652274184113224cf092c97df0a81c9c
alastor0325 <alastor0325@gmail.com>
2023-11-27 11:29:52 -0800

    update version to 122.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ import * as Visual from "./visualization.js";
   console.log(Visual);
   Visual.createPrioritySection();
 
-  const latestVersion = 120;
+  const latestVersion = 122;
   const displayVersionAmount = 5;
   for (let ver = latestVersion; ver > latestVersion - displayVersionAmount; ver--) {
     await Visual.getPieChartForFixedBugsFromVersion(ver);
